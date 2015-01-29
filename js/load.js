c435e891a3ad49b5a31363c52d94a7934b1e12f5
Margo <margo@cs.ioc.ee>
2015-01-29 09:49:04 +0200

    Top menu test

--- js/load.js
+++ js/load.js
@@ -1,5 +1,5 @@
 $(function() {
-			$('#top-nav').load('topmenu.html');
+			$('#top-nav').load('http://cocovila.github.io/topmenu.html');
 			$('#footer').load('http://cocovila.github.io/footer.html');
 
 });
